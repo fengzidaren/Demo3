d66c83fa1b3cc02c6dd245223abb70febd09761f
fengzidaren <1179281732@qq.com>
2018-03-23 12:36:40 +0800

    添加第三次修改

--- src/com/bawei/git/test/GitDemo.java
+++ src/com/bawei/git/test/GitDemo.java
@@ -4,7 +4,8 @@ public class GitDemo {
 
 	public static void main(String[] args) {
         System.out.println("hello world!!!");
-        System.out.println("hello world,two!!!");
+        System.out.println("hello world!!!");
+        System.out.println("hello world!!!");
 	}
 
 }
